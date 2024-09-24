7b8c6f673c07f18092f54e4aacd2750da402624f
Philip Deitz <philipd@spotify.com>
2024-09-23 21:46:41 -0400

    Generate code challenge

--- src/main/java/com/PD/authentication/SpotifyAuthorization.java
+++ src/main/java/com/PD/authentication/SpotifyAuthorization.java
@@ -1,13 +1,26 @@
 package com.PD.authentication;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Random;
 
 public class SpotifyAuthorization {
   private static final Random rng = new Random();
   private static final String POSSIBLE_VALUES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-~";
 
-  public static String computeAuthorizationCode() {
+  private SpotifyAuthorization() {}
+
+  public static String generateCodeVerifier() {
     return buildCode("", 128);
+  }
+
+  public static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+    byte[] data = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+    MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+    byte[] digest = messageDigest.digest(data);
+    return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
   }
 
   private static String buildCode(String code, int counter) {
--- src/test/java/com/PD/authentication/AuthenticatorTest.java
+++ src/test/java/com/PD/authentication/AuthenticatorTest.java
@@ -3,14 +3,21 @@ package com.PD.authentication;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.security.NoSuchAlgorithmException;
 import org.junit.jupiter.api.Test;
 
 class AuthenticatorTest {
   @Test
   void generateCodeVerifier() {
     String possibleValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-~";
-    String codeVerifier = SpotifyAuthorization.computeAuthorizationCode();
+    String codeVerifier = SpotifyAuthorization.generateCodeVerifier();
     assertEquals(128, codeVerifier.length());
     assertTrue(codeVerifier.chars().allMatch(c -> possibleValues.indexOf(c) != -1));
+  }
+
+  @Test
+  void generateCodeChallenge() throws NoSuchAlgorithmException {
+    String expected = "WZRHGrsBESr8wYFZ9sx0tPURuZgG2lmzyvWpwXPKz8U"; // Computed by code that I know works.
+    assertEquals(expected, SpotifyAuthorization.generateCodeChallenge("12345"));
   }
 }
