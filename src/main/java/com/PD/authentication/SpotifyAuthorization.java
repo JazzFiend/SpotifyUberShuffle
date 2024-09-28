acd995a52e04b0b182f2113843b72e1e20c55303
Philip Deitz <philipd@spotify.com>
2024-09-27 21:09:37 -0400

    Can now generate Authorization URL

--- src/main/java/com/PD/authentication/SpotifyAuthorization.java
+++ src/main/java/com/PD/authentication/SpotifyAuthorization.java
@@ -8,24 +8,41 @@ import java.util.Random;
 
 public class SpotifyAuthorization {
   private static final Random rng = new Random();
-  private static final String POSSIBLE_VALUES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-~";
+  private static final String POSSIBLE_VALUES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
 
   private SpotifyAuthorization() {}
 
-  public static String generateCodeVerifier() {
-    return buildCode("", 128);
+  public static String generateAuthorizationUrl(String clientId) throws NoSuchAlgorithmException {
+    StringBuilder result = new StringBuilder();
+    String state = generateRandomString(16);
+    String codeChallenge = generateCodeChallenge(generateRandomString(128));
+
+    result.append("https://accounts.spotify.com/authorize?");
+    result.append("client_id=").append(clientId).append("&");
+    result.append("response_type=code").append("&");
+    result.append("redirect_uri=http://localhost:8080").append("&");
+    result.append("state=").append(state).append("&");
+    result.append("scope=playlist-modify-private%20playlist-modify-public%20user-library-read").append("&");
+    result.append("code_challenge_method=S256").append("&");
+    result.append("code_challenge=").append(codeChallenge);
+
+    return result.toString();
   }
 
-  public static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+  private static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
     byte[] data = codeVerifier.getBytes(StandardCharsets.US_ASCII);
     MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
     byte[] digest = messageDigest.digest(data);
     return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
   }
 
-  private static String buildCode(String code, int counter) {
+  private static String generateRandomString(int size) {
+    return generateRandomStringRecursive("", size);
+  }
+
+  private static String generateRandomStringRecursive(String code, int counter) {
     if(counter == 0) { return code; }
 
-    return buildCode(code + POSSIBLE_VALUES.charAt(rng.nextInt(POSSIBLE_VALUES.length())), counter - 1);
+    return generateRandomStringRecursive(code + POSSIBLE_VALUES.charAt(rng.nextInt(POSSIBLE_VALUES.length())), counter - 1);
   }
 }
--- src/test/java/com/PD/authentication/AuthenticatorTest.java
+++ src/test/java/com/PD/authentication/AuthenticatorTest.java
@@ -4,20 +4,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class AuthenticatorTest {
-  @Test
-  void generateCodeVerifier() {
-    String possibleValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.-~";
-    String codeVerifier = SpotifyAuthorization.generateCodeVerifier();
-    assertEquals(128, codeVerifier.length());
-    assertTrue(codeVerifier.chars().allMatch(c -> possibleValues.indexOf(c) != -1));
-  }
+  private static final String CLIENT_ID = "CLIENT_ID";
 
   @Test
-  void generateCodeChallenge() throws NoSuchAlgorithmException {
-    String expected = "WZRHGrsBESr8wYFZ9sx0tPURuZgG2lmzyvWpwXPKz8U"; // Computed by code that I know works.
-    assertEquals(expected, SpotifyAuthorization.generateCodeChallenge("12345"));
+  void generateAuthorizationUrl() throws NoSuchAlgorithmException {
+    String authorizationUrl = SpotifyAuthorization.generateAuthorizationUrl(CLIENT_ID);
+
+    checkEndpoint(authorizationUrl);
+    checkParams(authorizationUrl);
+  }
+
+  private static void checkEndpoint(String authorizationUrl) {
+    String[] endpointAndParams = authorizationUrl.split("\\?");
+    assertEquals("https://accounts.spotify.com/authorize", endpointAndParams[0]);
+  }
+
+  private static void checkParams(String authorizationUrl) {
+    List<String> params = Arrays.stream(authorizationUrl.split("\\?")[1].split("&")).toList();
+    assertTrue(params.containsAll(List.of(
+        "client_id=" + CLIENT_ID,
+        "response_type=code",
+        "redirect_uri=http://localhost:8080",
+        "scope=playlist-modify-private%20playlist-modify-public%20user-library-read",
+        "code_challenge_method=S256"
+    )));
+    checkAllowedValues(params, "state");
+    checkAllowedValues(params, "code_challenge");
+  }
+
+  private static void checkAllowedValues(List<String> params, String paramName) {
+    String possibleValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-";
+    List<String[]> singleParam = params.stream()
+        .map(p -> p.split("="))
+        .filter(p -> p[0].equals(paramName))
+        .toList();
+    assertEquals(1, singleParam.size());
+    assertTrue(singleParam.get(0)[1].chars().allMatch(c -> possibleValues.indexOf(c) != -1));
   }
 }
