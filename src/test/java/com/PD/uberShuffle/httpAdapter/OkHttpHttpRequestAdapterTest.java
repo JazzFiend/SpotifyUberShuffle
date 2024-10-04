6e9c00807dccc5b2f01f35d76bc43b6d0b57f87f
Philip Deitz <philipd@spotify.com>
2024-10-03 21:04:25 -0400

    Replace mock caller with Mockito version

--- src/test/java/com/PD/uberShuffle/httpAdapter/OkHttpHttpRequestAdapterTest.java
+++ src/test/java/com/PD/uberShuffle/httpAdapter/OkHttpHttpRequestAdapterTest.java
@@ -1,15 +1,28 @@
 package com.PD.uberShuffle.httpAdapter;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.PD.mocks.OkHttpCallerMock;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.Request.Builder;
+import okhttp3.RequestBody;
 import org.json.JSONObject;
 import org.json.JSONWriter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 public class OkHttpHttpRequestAdapterTest {
   private final String accessToken = "Bearer 123";
@@ -90,32 +103,39 @@ public class OkHttpHttpRequestAdapterTest {
     assertEquals(actual.get("method"), expected.get("method"));
   }
 
-  // The headers should be working now. But thanks to the mock object, am I actually testing anything here?
   @Test
-  void postRequestWithHeadersTest() {
-    final String url = "http://www.postrequest.com/";
+  void postRequestWithHeadersTest() throws IOException {
+    OkHttpCaller mockCaller = mock();
+    when(mockCaller.makeRequest(any(Request.class))).thenReturn(new JSONObject());
 
-    StringWriter sw = new StringWriter();
-    new JSONWriter(sw)
-        .object()
-        .key("Authorization")
-        .value(accessToken)
-        .key("url")
-        .value(url)
-        .key("method")
-        .value("POST")
-        .endObject();
-    JSONObject expected = new JSONObject(sw.toString());
+    OkHttpHttpRequestAdapter httpWithMock = new OkHttpHttpRequestAdapter(mockCaller);
+
+    final String url = "https://www.postrequest.com/";
     Map<String, String> bodyParams = new HashMap<>();
     bodyParams.put("param", "value");
     Map<String, String> headers = new HashMap<>();
     headers.put("Authorization", accessToken);
     headers.put("AnotherHeader", "Value");
 
-    JSONObject actual = http.makePostRequest(url, bodyParams, headers);
+    final MediaType json = MediaType.get("application/json; charset=utf-8");
+    RequestBody body = RequestBody.create(new JSONObject(bodyParams).toString(), json);
+    Request expectedRequest = new Builder()
+        .post(body)
+        .header("Authorization", accessToken)
+        .header("AnotherHeader", "Value")
+        .url(url)
+        .build();
 
-    assertEquals(actual.get("Authorization"), expected.get("Authorization"));
-    assertEquals(actual.get("url"), expected.get("url"));
-    assertEquals(actual.get("method"), expected.get("method"));
+    httpWithMock.makePostRequest(url, bodyParams, headers);
+
+    ArgumentCaptor<Request> requestCaptor = ArgumentCaptor.forClass(Request.class);
+    verify(mockCaller, times(1)).makeRequest(requestCaptor.capture());
+    Request actualRequest = requestCaptor.getValue();
+
+    assertThat(expectedRequest.url(), is(actualRequest.url()));
+    assertThat(expectedRequest.method(), is(actualRequest.method()));
+    assertThat(expectedRequest.headers().toMultimap(), is(actualRequest.headers().toMultimap()));
+    assertThat(expectedRequest.body().contentType(), is(actualRequest.body().contentType()));
+    assertThat(expectedRequest.body().contentLength(), is(actualRequest.body().contentLength()));
   }
 }
