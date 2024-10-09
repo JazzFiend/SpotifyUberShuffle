1d70b3e345b8cfff4fd1655ab0804e32aeb10373
Philip Deitz <philipd@spotify.com>
2024-10-09 11:45:08 -0400

    Rename AuthenticationTest

--- src/test/java/com/PD/authentication/AuthenticationManualTest.java
+++ src/test/java/com/PD/authentication/AuthenticationManualTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class AuthenticationManualTest {
-  public static final String CLIENT_ID = "NOPE";
+  public static final String CLIENT_ID = "Replace with client id";
 
   @ParameterizedTest
   @ValueSource(ints = {1, 5, 43, 128})
@@ -27,10 +27,10 @@ class AuthenticationManualTest {
   @Test
   void authorizationCurl() throws NoSuchAlgorithmException {
     String codeVerifier = Authentication.generateRandomString(128);
-    String authorizationURL = Authentication.generateAuthorizationCurl(codeVerifier, CLIENT_ID);
+    String authorizationCurl = Authentication.generateAuthorizationCurl(codeVerifier, CLIENT_ID);
     System.out.println(codeVerifier);
-    System.out.println(authorizationURL);
-    assertNotNull(authorizationURL);
+    System.out.println(authorizationCurl);
+    assertNotNull(authorizationCurl);
   }
   @Test
   void accessTokenCurl() {
